b41f9dbdde12a25bb6505e20537e4bae9c33a73b
YvonneCollingham <NBuser@DL-BSC-150.mds.ad.dur.ac.uk>
2016-06-28 16:34:24 +0100

    data.py now dynamic
    outputs client status

--- data.py
+++ data.py
@@ -1,17 +1,38 @@
 import sys
-
+Status = sys.argv[1]
 f=open("primer.csv","r")
-print f
-total = 0
-LineNum = 0
+#print f
+TotalBalance = 0
+TotalCost = 0
+TotalPaid = 0
 for line in f:
-	#line = line.strip('\n').strip('\r')
-	line = line.strip('\n')
-	entry = line.split(',')
-	#print entry
-	#print entry[4]
-	if (LineNum>0):
-		total = total + int(entry[4])
-	LineNum=LineNum+1
-print "Balance total = ", total
+	try:
+		#line = line.strip('\n').strip('\r')
+		line = line.strip('\n')
+		entry = line.split(',')
+		TotalBalance += int(entry[4])
+		TotalCost += int(entry[2])
+		TotalPaid += int(entry[3])
+	except:
+		print "bad line ", line
+		#pass
+print "\nBalance total = ", TotalBalance
+print "Cost total = ", TotalCost
+print "Paid total", TotalPaid
+
+
+#f.close
+#f=open("primer.csv","r")
+f.seek(0)
+print "\nStatus " + Status
+
+for line in f:
+	try:
+		line = line.strip('\n')
+		entry = line.split(',')
+		if entry[5]==Status:
+			print entry[1]
+	except:
+		pass
 f.close
+

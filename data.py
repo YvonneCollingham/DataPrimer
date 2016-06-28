56d9d11fb832bb18c2dc7d498290d5e14b25326f
YvonneCollingham <NBuser@DL-BSC-150.mds.ad.dur.ac.uk>
2016-06-28 16:54:10 +0100

    data.py dynamic
    uses fseek(0) instead of opening and closing input file

--- data.py
+++ data.py
@@ -25,14 +25,23 @@ print "Paid total", TotalPaid
 #f=open("primer.csv","r")
 f.seek(0)
 print "\nStatus " + Status
-
+count = 0
+TotalBalance = 0
+TotalCost = 0
+TotalPaid = 0
 for line in f:
 	try:
 		line = line.strip('\n')
 		entry = line.split(',')
 		if entry[5]==Status:
 			print entry[1]
+			count +=1
+			TotalBalance += int(entry[4])
+			TotalCost += int(entry[2])
+			TotalPaid += int(entry[3])
 	except:
 		pass
-f.close
-
+print "\nCases that are " + Status + " = " + str(count)
+print Status + " Balance total = ", TotalBalance
+print Status + " Cost total = ", TotalCost
+print Status + " Paid total", TotalPaid

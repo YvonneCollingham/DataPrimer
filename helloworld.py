16cf037d682645cb40962f6c701bbe8e22a78088
YvonneCollingham <NBuser@DL-BSC-150.mds.ad.dur.ac.uk>
2016-06-28 15:07:04 +0100

    fix output for hello world

--- helloworld.py
+++ helloworld.py
@@ -7,5 +7,5 @@ print "Hello " + personName
 TimeTable = int(sys.argv[2])
 NumSteps = int(sys.argv[3])
 for i in range (1,NumSteps):
-	print str(TimeTable) + " x " + str(i) + " = " str(TimeTable*i)
-	
+	#print str(TimeTable) + " x " + str(i) + " = " str(TimeTable*i)
+	print TimeTable*i

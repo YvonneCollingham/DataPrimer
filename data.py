ef9698bb35268610a33850f9d0960147a6f39295
YvonneCollingham <NBuser@DL-BSC-150.mds.ad.dur.ac.uk>
2016-06-29 10:55:47 +0100

    data.py with function
    function now added

--- data.py
+++ data.py
@@ -1,47 +1,79 @@
 import sys
-Status = sys.argv[1]
-f=open("primer.csv","r")
-#print f
-TotalBalance = 0
-TotalCost = 0
-TotalPaid = 0
-for line in f:
-	try:
-		#line = line.strip('\n').strip('\r')
-		line = line.strip('\n')
-		entry = line.split(',')
-		TotalBalance += int(entry[4])
-		TotalCost += int(entry[2])
-		TotalPaid += int(entry[3])
-	except:
-		print "bad line ", line
-		#pass
-print "\nBalance total = ", TotalBalance
-print "Cost total = ", TotalCost
-print "Paid total", TotalPaid
+
+def printnCalc(record,newTotal,newPaid,newBalance):
+	print record[1] + " " + record[2] + " " + record[3] + " " + record[4] + " "
+	newTotal += int(record[2])
+	newPaid += int(record[3])
+	newBalance += int(record[4])
+	return newTotal,newPaid,newBalance
 
 
-#f.close
-#f=open("primer.csv","r")
-f.seek(0)
-print "\nStatus " + Status
-count = 0
-TotalBalance = 0
-TotalCost = 0
-TotalPaid = 0
+#### Main Program
+f=open("data.csv","r")
+
+
+runningTotal=0
+runningPaid=0
+runningBalance=0
+
 for line in f:
+	line=line.strip('\n').strip('\r')
+	entry=line.split(',')
 	try:
-		line = line.strip('\n')
-		entry = line.split(',')
-		if entry[5]==Status:
-			print entry[1]
-			count +=1
-			TotalBalance += int(entry[4])
-			TotalCost += int(entry[2])
-			TotalPaid += int(entry[3])
+		runningTotal += int(entry[2])
+		runningPaid += int(entry[3])
+		runningBalance += int(entry[4])
 	except:
 		pass
-print "\nCases that are " + Status + " = " + str(count)
-print Status + " Balance total = ", TotalBalance
-print Status + " Cost total = ", TotalCost
-print Status + " Paid total", TotalPaid
+
+print "Total Cost = " + str(runningTotal) 
+print "Total Paid = " + str(runningPaid)
+print "Total Balance = " + str(runningBalance)
+
+f.seek(0)
+
+analysis=sys.argv[1]
+
+runningTotal=0
+runningPaid=0
+runningBalance=0
+
+for line in f:
+	line=line.strip('\n').strip('\r')
+	entry=line.split(',')
+
+	if analysis=="closed":
+		try:
+			if int(entry[4])==0:
+				runningTotal,runningPaid,runningBalance=printnCalc(entry,runningTotal,runningPaid,runningBalance)
+		except:
+			pass
+	elif analysis=="pending":
+		try:
+			if int(entry[3])==0:
+				runningTotal,runningPaid,runningBalance=printnCalc(entry,runningTotal,runningPaid,runningBalance)
+		except:
+			pass
+	elif analysis=="open":
+		try:
+			if (int(entry[2])>int(entry[3])) and (int(entry[3])!=0):
+				runningTotal,runningPaid,runningBalance=printnCalc(entry,runningTotal,runningPaid,runningBalance)
+		except:
+			pass
+
+print "==================="
+
+	#if entry[5]==analysis:
+	#	print entry[1] + " " + entry[2] + " " + entry[3] + " " + entry[4] + " "
+	#	runningTotal += int(entry[2])
+	#	runningPaid += int(entry[3])
+	#	runningBalance += int(entry[4])
+
+print analysis + " Total = " + str(runningTotal)
+print analysis + " Paid = " + str(runningPaid)	
+print analysis + " Balance = " + str(runningBalance)
+
+f.close
+
+
+
